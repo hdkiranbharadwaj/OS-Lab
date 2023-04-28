24211524a507d39d351d306bcca21a5077465dae
hdkiranbharadwaj <104179274+hdkiranbharadwaj@users.noreply.github.com>
2023-04-28 12:21:47 +0530

    Create Fibonacci.sh

---
+++ Fibonacci.sh
@@ -0,0 +1,35 @@
+#printing n fibonacci numbers
+
+#!/bin/bash
+
+
+
+n1=0
+
+n2=1
+
+echo "Enter the number of fibonacci numbers"
+
+read n
+
+echo "the first $n fibonacci numbers are : "
+
+echo
+
+echo $n1
+
+echo $n2
+
+for((i = 2;i < $n;i++))
+
+do
+
+	n3=$((n1+n2))
+
+	echo $n3
+
+	n1=$n2
+
+	n2=$n3
+
+done

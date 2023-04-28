97c7379d59853d3b142217ac3b8c314f1e0e4a63
hdkiranbharadwaj <104179274+hdkiranbharadwaj@users.noreply.github.com>
2023-04-28 12:19:52 +0530

    Create greatestofthree.sh

---
+++ greatestofthree.sh
@@ -0,0 +1,13 @@
+#Greatest of three numbers
+#!/bin/bash
+
+echo "Enter three number : "
+read n1 n2 n3
+if [[ "$n1" >  "$n2" && "$n1" >  "$n3" ]]
+then echo "$n1 is greatest"
+else if [[ "$n2" >  "$n1" && "$n2" >  "$n3" ]] 
+then echo "$n2 is the greatest"
+else
+	echo "$n3 is the greatest"	
+fi
+fi

b31890f88074f79535cdb305e358008b6a28dba4
hdkiranbharadwaj <104179274+hdkiranbharadwaj@users.noreply.github.com>
2023-05-12 11:14:19 +0530

    Update palindrome_no.sh

--- palindrome_no.sh
+++ palindrome_no.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Enter the number "
 read n 
-temp = $n
-while((temp>0))
+temp=$n
+while((temp > 0))
 do
   r=$((temp%10))
   rev=$((rev*10+r))
   temp=$((temp/10))
 done
-if[ "$temp" = "$n" ]
+if [ "$rev" = "$n" ]
 then echo "It is a palindrome"
 else echo "It is not a palindrome"
 fi
